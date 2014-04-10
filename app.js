e961048716045fa94d6458ab91d54828e74a5246
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2014-04-10 09:32:57 -0500

    update to express 4.x

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 /**
- * NodeIpaServer
+ * Node-ipa-server
  * Oscar Sobrevilla oscar.sobrevilla@gmail.com
  */
-require('./setup').install();
+
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
+var routes = require('./routes');
+var users = require('./routes/user');
+
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
+// App routers 
 app.get('/', routes.index);
 app.get('/files/new', routes.create);
 app.get('/files/download/:id', routes.download);
@@ -38,8 +40,33 @@ app.get('/files/:id/:slug', routes.one);
 app.put('/files/update/:id', routes.update);
 app.post('/files', multipartMiddleware, routes.save);
 
-
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
